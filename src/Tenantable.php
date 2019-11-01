07a05a24233ddb3ad9224c98deb045f785d25f5e
Jamiel <20553479+jamielsharief@users.noreply.github.com>
2019-11-01 16:37:55 +0100

    Refactored to use callbacks to provide greater compatibility with other concerns

--- src/Tenantable.php
+++ src/Tenantable.php
@@ -11,76 +11,73 @@
  * @link        https://www.originphp.com
  * @license     https://opensource.org/licenses/mit-license.php MIT License
  */
+declare(strict_types = 1);
 namespace MultiTenant;
 
+use ArrayObject;
 use Origin\Model\Entity;
 
 /**
- * Multitenant Concern - sidestepping callbacks
+ * Multi-tenant Concern
  */
 trait Tenantable
 {
     /**
-     * Runs a find query
+     * Register callbacks
      *
-     * @param string $type  (first,all,count,list)
-     * @param array $options  The options array can work with the following keys
-     *   - conditions: an array of conditions to find by. e.g ['id'=>1234,'status !=>'=>'new]
-     *   - fields: an array of fields to fetch for this model. e.g ['id','title','description']
-     *   - joins: an array of join arrays e.g. table' => 'authors','alias' => 'authors', 'type' => 'LEFT' ,
-     * 'conditions' => ['authors.id = articles.author_id']
-     *   - order: the order to fetch e.g. ['title ASC'] or ['category','title ASC']
-     *   - limit: the number of records to limit by
-     *   - group: the field to group by e.g. ['category']
-     *   - callbacks: default is true. Set to false to disable running callbacks such as beforeFind and afterFind
-     *   - associated: an array of models to get data for e.g. ['Comment'] or ['Comment'=>['fields'=>['id','body']]]
-     * @return \Origin\Model\Entity|\Origin\Model\Collection|array|int $resultSet
+     * @return void
      */
-    public function find(string $type = 'first', $options = [])
+    protected function initializeTenantable(): void
+    {
+        $this->beforeFind('tenantBeforeFind');
+        $this->beforeCreate('tenantBeforeCreate');
+        $this->beforeDelete('tenantBeforeDelete');
+    }
+
+    /**
+     * Adds the TenantId to the find conditions
+     *
+     * @param ArrayObject $options
+     * @return void
+     */
+    protected function tenantBeforeFind(ArrayObject $options) : void
     {
         if (Tenant::initialized()) {
             $foreignKey = Tenant::foreignKey();
             $options['conditions'][$foreignKey] = Tenant::id();
         }
-       
-        return parent::find($type, $options);
     }
 
     /**
-    * Save model data to database, it can save one level of associations.
-    *
-    * ## Options
-    *
-    * The options array can be passed with the following keys:
-    *
-    * - validate: wether to validate data or not
-    * - callbacks: call the callbacks duing each stage.  You can also put only before or after
-    * - transaction: wether to save through a database transaction (default:true)
-    * - associated: default true. boolean or an array of associated data to save as well
-    *
-    * # Callbacks
-    *
-    * The following callbacks will called in this Model
-    *
-    * - beforeValidate
-    * - afterValidate
-    * - beforeSave
-    * - beforeCreate/beforeCreate
-    * - afterCreate/afterUpdate
-    * - afterSave
-    * - afterCommit/afterRollback
-    *
-    * @param entity $entity to save
-    * @param array  $options keys (validate,callbacks,transaction,associated)
-    * @return bool true or false
-    */
-    public function save(Entity $data, array $options = []) : bool
+     * Adds the TenantId to new records before they are saved
+     *
+     * @param \Origin\Model\Entity $data
+     * @param ArrayObject $options
+     * @return void
+     */
+    protected function tenantBeforeCreate(Entity $data, ArrayObject $options) : void
     {
-        if (Tenant::initialized() and ! $data->exists()) {
+        if (Tenant::initialized()) {
             $foreignKey = Tenant::foreignKey();
             $data->$foreignKey = Tenant::id();
         }
-  
-        return parent::save($data);
+    }
+
+    /**
+     * Checks the foreign key that is being deleted
+     *
+     * @param \Origin\Model\Entity $data
+     * @param ArrayObject $options
+     * @return void
+     */
+    protected function tenantBeforeDelete(Entity $data, ArrayObject $options) : bool
+    {
+        if (Tenant::initialized()) {
+            $foreignKey = Tenant::foreignKey();
+
+            return $data->$foreignKey === Tenant::id();
+        }
+
+        return true;
     }
 }
